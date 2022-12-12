a08df504f5e4d7c0725590e889fd9fadf697521c
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2022-12-12 15:31:51 +0100

    Update deleteStream.js

--- src/superfluidFunctions/deleteStream.js
+++ src/superfluidFunctions/deleteStream.js
@@ -4,19 +4,19 @@ import { ethers } from "ethers";
 export default async function deleteFlow(recipient) {
     const provider = new ethers.providers.JsonRpcProvider(
         process.env.NODE_ENV_INFURA_URL,  //Your Infura NETWORK ENDPOINTS
-        137
+        56
       );
 
     const signer = new ethers.Wallet(process.env.NODE_ENV_PRIVATE_KEY, provider);
 
     const chainId = await window.ethereum.request({ method: "eth_chainId" });
     const superfluid = await Framework.create({
-        chainId: 137,
+        chainId: 56,
         provider: provider,
     });
 
-    const NEARxContract = await superfluid.loadSuperToken("0x094Ed09F072596C34C5c0b197dcEB0da6b04C580");
-    const NEARx = NEARxContract.address;
+    const lisprocoinContract = await superfluid.loadSuperToken("0xE62A9bc6eDe534E18Dd2793Dcaf5A2B6df112180");
+    const lisprocoin = lisprocoinContract.address;
 
     const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
 
@@ -33,9 +33,9 @@ export default async function deleteFlow(recipient) {
 
         console.log(
             `Congrats - you've just deleted your money stream!
-            Network: Polygon
-            Super Token: NEARx
-            Sender: 0x6EeE6060f715257b970700bc2656De21dEdF074C
+            Network: Binance
+            Super Token: lisprocoin
+            Sender: 0xE62A9bc6eDe534E18Dd2793Dcaf5A2B6df112180
             Receiver: ${recipient}
             `
         );
