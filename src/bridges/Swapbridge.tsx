5ca23fab65fb33b5fa0933cad641dd1d2669b8ba
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2022-12-12 22:24:15 +0100

    Update Swapbridge.tsx

--- src/bridges/Swapbridge.tsx
+++ src/bridges/Swapbridge.tsx
@@ -3,7 +3,7 @@ import { LiFiWidget, WidgetConfig } from '@lifi/widget';
 const widgetConfig: WidgetConfig = {
   integrator: 'Perseus',
   toChain: 137,
-  toToken: '	0x2776cAFe6dcAeB292A013Cb03e3aB332DAa52e8F',
+  toToken: '0x2776cAFe6dcAeB292A013Cb03e3aB332DAa52e8F',
   containerStyle: {
     border: '1px solid rgb(234, 234, 234)',
     borderRadius: '16px',
