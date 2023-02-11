247e143cb400fe3838e71786e701f2ffa31f71a2
Lisprocoin <83799681+15-Lippo@users.noreply.github.com>
2023-02-11 23:26:30 +0100

    Update Bridgeswap.tsx

--- src/bridges/Bridgeswap.tsx
+++ src/bridges/Bridgeswap.tsx
@@ -1,17 +1,9 @@
 import { LiFiWidget, WidgetConfig } from '@lifi/widget';
 
 const widgetConfig: WidgetConfig = {
-  integrator: 'wmatic',
+  integrator: 'Lisprocoin',
   fromChain: 137,
-  fromToken: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
-  containerStyle: {
-    border: '1px solid rgb(234, 234, 234)',
-    borderRadius: '16px',
-
-const widgetConfig: WidgetConfig = {
-  integrator: 'lisprocoin',
-  fromChain: 137,
-  ToToken: '0x70e546c7a2ca4495cfcbe263a3b6d5ce68b2204c',
+  To Token: '0x70e546c7a2ca4495cfcbe263a3b6d5ce68b2204c',
   containerStyle: {
     border: '1px solid rgb(234, 234, 234)',
     borderRadius: '16px',
